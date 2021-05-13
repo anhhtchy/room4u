6f8788d7c4ade717637f208f10053b4f52ea8280
Ly Trung Kien <lytrungkientq99@gmail.com>
2021-05-13 12:10:34 +0700

    + add modal Add, Edit, Delete

--- r4y_frontend/src/components/KenhChuNha/components/Tab1.js
+++ r4y_frontend/src/components/KenhChuNha/components/Tab1.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
+import axios from "axios";
 
-import { Avatar, Button, Modal } from 'antd';
+import {
+    Avatar,
+    Button,
+    Modal,
+    message,
+    Form,
+    Input,
+    Select,
+    notification,
+    Upload,
+    Rate,
+} from 'antd';
+
 import {
     SendOutlined,
     EnvironmentOutlined,
@@ -10,12 +23,18 @@ import {
     FieldTimeOutlined,
     HeartOutlined,
     EditOutlined,
+    PlusOutlined,
+    LoadingOutlined,
 } from '@ant-design/icons';
 
 import styles from './tab.module.css';
 
+const { Option } = Select;
+const loadingIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
 const Tab1 = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [uploading, setUploading] = useState(false);
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -29,6 +48,16 @@ const Tab1 = () => {
         setIsModalVisible(false);
     };
 
+    const onFinish = (values) => {
+        console.log("edit", values);
+        setIsModalVisible(false);
+        message.success('Đã lưu!');
+    }
+
+    const onFinishFailed = (errInfo) => {
+        console.log("edit err", errInfo);
+    }
+
     return (
         <div className={styles.tab}>
             <div className={styles.tabTitle}>
@@ -36,12 +65,271 @@ const Tab1 = () => {
                 <Button className={styles.button} icon={<EditOutlined />} onClick={showModal}>Sửa thông tin</Button>
             </div>
             <Modal
-                title="Chỉnh sửa thông tin cá nhân"
+                title={<div
+                    style={{
+                        fontSize: '20px',
+                        textAlign: 'center',
+                        color: '#faad14',
+                    }}
+                >CHỈNH SỬA THÔNG TIN CÁ NHÂN</div>}
+                width={700}
                 visible={isModalVisible}
                 onOk={handleOk}
                 onCancel={handleCancel}
+                okText="Đăng"
+                cancelText="Hủy"
+                footer={null}
             >
-                <p>Some contents...</p>
+                <Form
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}
+                >
+                    {/* <div style={{ marginBottom: '6px' }}>Tên người dùng: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="Tên người dùng"
+                        name="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập tên người dùng!',
+                            },
+                            {
+                                type: 'string',
+                                message: 'Tên người dùng chỉ chứa kí tự chữ!',
+                            },
+                            {
+                                max: 30,
+                                message: 'Tên người dùng không dài quá 30 kí tự',
+                            }
+                        ]}
+                    >
+                        <Input value="Hoàng Thế Anh" />
+                    </Form.Item>
+
+                    {/* <div style={{ marginBottom: '6px' }}>Tên đăng nhập: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="Tên đăng nhập"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập tên đăng nhập!',
+                            },
+                            {
+                                max: 20,
+                                message: 'Tên đăng nhập không dài quá 20 kí tự',
+                            }
+                        ]}
+                    >
+                        <Input value="abc20202" />
+                    </Form.Item>
+
+                    {/* <div style={{ marginBottom: '6px' }}>Email: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="email"
+                        name="email"
+                        rules={[
+                            {
+                                type: 'email',
+                                message: 'Email không hợp lệ!',
+                            },
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập email của bạn!',
+                            },
+                        ]}
+                    >
+                        <Input value="abc@gmail/com" />
+                    </Form.Item>
+
+                    {/* <div style={{ marginBottom: '6px' }}>SĐT: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="SĐT"
+                        name="phone"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập số điện thoại của bạn!',
+                            },
+                            {
+                                pattern: '([0-9])',
+                                message: 'Vui lòng nhập số!'
+                            },
+                            {
+                                max: 10,
+                                message: 'SĐT không dài quá 10 kí tự!',
+                            }
+                        ]}
+                    >
+                        <Input style={{ width: '100%' }} value="0961274321" />
+                    </Form.Item>
+
+                    {/* <div style={{ marginBottom: '6px' }}>Thay đổi mật khẩu: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="Thay đổi mật khẩu"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập mật khẩu của bạn!',
+                            },
+                            {
+                                min: 6,
+                                message: 'Độ dài mật khẩu tối thiểu là 6 kí tự',
+                            }
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    {/* <div style={{ marginBottom: '6px' }}>Xác nhận lại mật khẩu: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="Xác nhận mật khẩu"
+                        name="confirm"
+                        dependencies={['password']}
+                        hasFeedback
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng xác nhận lại mật khẩu!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject(new Error('Mật khẩu không khớp!'));
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    {/* <div>Địa chỉ: <span style={{ color: '#f5222d' }}>*</span></div> */}
+                    <Form.Item
+                        label="Địa chỉ"
+                        name="address"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Vui lòng nhập địa chỉ của bạn!',
+                            },
+                            {
+                                max: 200,
+                                message: 'Địa chỉ không dài quá 200 kí tự',
+                            }
+                        ]}
+                    >
+                        <Input value="Số 1, đường Đại Cồ Việt, Hai Bà Trưng, Hà Nội " />
+                    </Form.Item>
+
+
+
+                    <span style={{ marginRight: '40px' }}> <span style={{ color: '#f5222d' }}>*</span>Chọn ảnh đại diện:</span>
+                    <Upload
+                        style={{ marginTop: '10px' }}
+                        beforeUpload={(file) => {
+                            console.log(file);
+                            if (!file.type.includes('image/')) {
+                                notification.error({
+                                    message: `${file.name} is not a image file`,
+                                });
+                            }
+                            return file.type.includes('image/');
+                        }}
+                        onChange={async (info) => {
+                            if (info.file.status === 'uploading') {
+                                console.log(info.file.status);
+                                setUploading(true);
+                            } else {
+                                console.log(info.file.status);
+                                setUploading(false);
+                            }
+                            if (info.file.status === 'done') {
+                                console.log(info.fileList);
+                                // setFileList(info.fileList);
+                            } else if (info.file.status === 'error') {
+                                console.log(info.file);
+                                notification.error({
+                                    message: info.file.error?.response?.data?.error?.message
+                                        ? info.file.error.response.data.error.message
+                                        : 'An error occurred',
+                                });
+                            }
+                        }}
+                        accept="image/*"
+                        multiple={true}
+                        showUploadList={false}
+                        customRequest={async ({ onSuccess, onError, file }) => {
+                            const form = new FormData();
+                            form.append('image', file);
+                            try {
+                                const res = await axios.post(
+                                    "http://localhost:3001/upload",
+                                    form,
+                                );
+                                if (res.data.status === 200) {
+                                    file.url = res.data.data.url;
+                                    onSuccess(null, file);
+                                } else {
+                                    onError(res.data.error.message);
+                                }
+                            } catch (error) {
+                                onError(error);
+                            }
+                        }}
+                    >
+                        <div className={styles.upload}>
+                            {uploading ? (
+                                loadingIcon
+                            ) : (
+                                <>
+                                    <PlusOutlined />
+                                    <div style={{ marginTop: 8 }}>
+                                        Upload
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </Upload>
+
+                    <Form.Item style={{ marginTop: '30px', textAlign: 'right' }}>
+                        <Button className={styles.buttonOutline}
+                            onClick={handleCancel}
+                            style={{
+                                width: '20%',
+                                marginRight: '20px',
+                                border: '1px solid #141414',
+                                color: '#141414',
+                                height: '48px',
+                                padding: '6px 30px',
+                                fontWeight: '500',
+                                fontSize: '16px',
+                                borderRadius: '8px',
+                            }}>
+                            HỦY
+                        </Button>
+                        <Button type="primary" htmlType="submit" className={styles.button}
+                            style={{
+                                width: '20%',
+                                marginRight: '20px',
+                                border: '1px solid #faad14',
+                                height: '48px',
+                                padding: '6px 30px',
+                                fontWeight: '500',
+                                fontSize: '16px',
+                                borderRadius: '8px',
+                                background: '#faad14',
+                                color: 'white',
+                            }}
+                        >
+                            LƯU
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Modal>
             <div className={styles.content}>
                 <div className={styles.leftContent}>
@@ -82,7 +370,16 @@ const Tab1 = () => {
                     </div>
                     <div className={styles.item}>
                         <div className={styles.itemTitle}><HeartOutlined style={{ marginRight: '5px' }} />Đánh giá:</div>
-                        <div className={styles.itemValue}>abc@gmail.com</div>
+                        <div className={styles.itemValue}>
+                            <div className={styles.itemRating}>
+                                <Rate
+                                    allowHalf
+                                    disabled
+                                    defaultValue={4}
+                                    style={{ fontSize: '14px', color: '#faad14' }}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- r4y_frontend/src/components/KenhChuNha/components/Tab2.js
+++ r4y_frontend/src/components/KenhChuNha/components/Tab2.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
+import axios from "axios";
 
-import { Button, Row, Col, Modal } from 'antd';
+import {
+    Button,
+    Row,
+    Col,
+    Modal,
+    message,
+    Form,
+    Input,
+    Select,
+    notification,
+    Upload,
+} from 'antd';
+
 import {
     EditOutlined,
     PlusCircleOutlined,
+    PlusOutlined,
+    LoadingOutlined,
 } from '@ant-design/icons';
 
 import Item from './Item1';
@@ -15,6 +30,9 @@ import img from "../../../img/img.jpg";
 import img1 from "../../../img/img1.jpg";
 import img2 from "../../../img/img2.jpg";
 import img3 from "../../../img/img3.jpg";
+
+const { Option } = Select;
+const loadingIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
 const mockData = [
     {
@@ -86,6 +104,7 @@ const mockData = [
 
 const Tab2 = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [uploading, setUploading] = useState(false);
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -93,11 +112,22 @@ const Tab2 = () => {
 
     const handleOk = () => {
         setIsModalVisible(false);
+        message.success('Đăng bài thành công!');
     };
 
     const handleCancel = () => {
         setIsModalVisible(false);
     };
+    
+    const onFinish = (values) => {
+        console.log("edit", values);
+        setIsModalVisible(false);
+        message.success('Đăng bài thành công!');
+    }
+
+    const onFinishFailed = (errInfo) => {
+        console.log("edit err", errInfo);
+    }
 
     return (
         <div className={styles.tab}>
@@ -131,13 +161,215 @@ const Tab2 = () => {
             </div>
 
             <Modal
-                title="Đăng tin mới"
-                width={800}
+                title={<div
+                    style={{
+                        fontSize: '20px',
+                        textAlign: 'center',
+                        color: '#faad14',
+                    }}
+                >ĐĂNG TIN MỚI</div>}
+                width={700}
                 visible={isModalVisible}
                 onOk={handleOk}
                 onCancel={handleCancel}
+                okText="Đăng"
+                cancelText="Hủy"
+                footer={null}
             >
-                <p>Some contents...</p>
+                <Form
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        name="title"
+                        rules={[{ required: true, message: 'Vui lòng nhập tiêu đề!' }]}
+                    >
+                        <div style={{ marginBottom: '6px', fontWeight: '500' }}>Tiêu đề: <span style={{ color: '#f5222d' }}>*</span></div>
+                        <Input
+                            placeholder="Nhập tiêu đề bài viết"
+                            size="large"
+                        />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="type"
+                        rules={[{ required: true, message: 'Vui lòng chọn loại bất động sản!' }]}
+                    >
+                        <div style={{ marginBottom: '6px', fontWeight: '500' }}>Loại bất động sản: <span style={{ color: '#f5222d' }}>*</span></div>
+                        <Select
+                            placeholder="Chọn loại BĐS"
+                            allowClear
+                            size="large"
+                            style={{ width: 'calc(50% - 8px)' }}
+                        >
+                            <Option value="Phòng trọ sinh viên">Phòng trọ sinh viên</Option>
+                            <Option value="Chung cư">Chung cư</Option>
+                            <Option value="Chung cư mini">Chung cư mini</Option>
+                            <Option value="Nhà nguyên căn">Nhà nguyên căn</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="price"
+                        rules={[{ required: true, message: 'Vui lòng nhập giá phòng!' }]}
+                    >
+                        <div style={{ marginBottom: '6px', fontWeight: '500' }}>Giá phòng: <span style={{ color: '#f5222d' }}>*</span></div>
+                        <Input
+                            placeholder="Nhập giá phòng"
+                            size="large"
+                            type="number"
+                            style={{ width: 'calc(50% - 8px)', marginRight: '10px' }}
+                        />
+                        <span>(đồng/tháng)</span>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="address"
+                        rules={[{ required: true, message: 'Vui lòng nhập địa chỉ!' }]}
+                    >
+                        <div style={{ marginBottom: '6px', fontWeight: '500' }}>Địa chỉ: <span style={{ color: '#f5222d' }}>*</span></div>
+                        <Input
+                            placeholder="Nhập địa chỉ BĐS"
+                            size="large"
+                        />
+                    </Form.Item>
+
+                    <div style={{ marginBottom: '6px', fontWeight: '500' }}>Giá dịch vụ: <span style={{ color: '#f5222d' }}>*</span></div>
+                    <Form.Item
+                        label="Giá điện"
+                        name="elecPrice"
+                        rules={[{ required: true, message: 'Vui lòng nhập giá điện!' }]}
+                        labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}
+                    >
+                        <Input
+                            size="large"
+                            type="number"
+                            style={{ width: 'calc(50% - 8px)', marginRight: '8px' }}
+                        />
+                        <span>(đồng/tháng)</span>
+                    </Form.Item>
+                    <Form.Item
+                        label="Giá nước"
+                        name="waterPrice"
+                        rules={[{ required: true, message: 'Vui lòng nhập giá nước!' }]}
+                        labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}
+                    >
+                        <Input
+                            size="large"
+                            type="number"
+                            style={{ width: 'calc(50% - 8px)', marginRight: '8px' }}
+                        />
+                        <span>(đồng/tháng)</span>
+                    </Form.Item>
+                    <Form.Item
+                        label="Giá wifi"
+                        name="wifiPrice"
+                        rules={[{ required: true, message: 'Vui lòng nhập giá wifi!' }]}
+                        labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}
+                    >
+                        <Input
+                            size="large"
+                            type="number"
+                            style={{ width: 'calc(50% - 8px)', marginRight: '8px' }}
+                        />
+                        <span>(đồng/tháng)</span>
+                    </Form.Item>
+                    <Form.Item
+                        label="Giá gửi xe"
+                        name="packingPrice"
+                        rules={[{ required: true, message: 'Vui lòng nhập giá gửi xe!' }]}
+                        labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}
+                    >
+                        <Input
+                            size="large"
+                            type="number"
+                            style={{ width: 'calc(50% - 8px)', marginRight: '8px' }}
+                        />
+                        <span>(đồng/tháng)</span>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="description"
+                        rules={[{ required: true, message: 'Vui lòng nhập mô tả!' }]}
+                    >
+                        <div style={{ marginBottom: '6px', fontWeight: '500' }}>Mô tả: <span style={{ color: '#f5222d' }}>*</span></div>
+                        <Input.TextArea rows={8} />
+                    </Form.Item>
+
+                    <div style={{ marginBottom: '6px', fontWeight: '500' }}>Thêm ảnh: <span style={{ color: '#f5222d' }}>*</span></div>
+                    <Upload
+                        style={{ marginTop: '10px' }}
+                        beforeUpload={(file) => {
+                            console.log(file);
+                            if (!file.type.includes('image/')) {
+                                notification.error({
+                                    message: `${file.name} is not a image file`,
+                                });
+                            }
+                            return file.type.includes('image/');
+                        }}
+                        onChange={async (info) => {
+                            if (info.file.status === 'uploading') {
+                                console.log(info.file.status);
+                                setUploading(true);
+                            } else {
+                                console.log(info.file.status);
+                                setUploading(false);
+                            }
+                            if (info.file.status === 'done') {
+                                console.log(info.fileList);
+                                // setFileList(info.fileList);
+                            } else if (info.file.status === 'error') {
+                                console.log(info.file);
+                                notification.error({
+                                    message: info.file.error?.response?.data?.error?.message
+                                        ? info.file.error.response.data.error.message
+                                        : 'An error occurred',
+                                });
+                            }
+                        }}
+                        accept="image/*"
+                        multiple={true}
+                        showUploadList={false}
+                        customRequest={async ({ onSuccess, onError, file }) => {
+                            const form = new FormData();
+                            form.append('image', file);
+                            try {
+                                const res = await axios.post(
+                                    "http://localhost:3001/upload",
+                                    form,
+                                );
+                                if (res.data.status === 200) {
+                                    file.url = res.data.data.url;
+                                    onSuccess(null, file);
+                                } else {
+                                    onError(res.data.error.message);
+                                }
+                            } catch (error) {
+                                onError(error);
+                            }
+                        }}
+                    >
+                        <div className={styles.upload}>
+                            {uploading ? (
+                                loadingIcon
+                            ) : (
+                                <>
+                                    <PlusOutlined />
+                                    <div style={{ marginTop: 8 }}>
+                                        Upload
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </Upload>
+
+                    <Form.Item style={{ marginTop: '20px', textAlign: 'center' }}>
+                        <Button type="primary" htmlType="submit" className={styles.button}>
+                            ĐĂNG
+                        </Button>
+                    </Form.Item>
+                </Form>
             </Modal>
         </div>
     )
